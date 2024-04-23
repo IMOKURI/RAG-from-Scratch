b58d90086308dda55958dab5882e18078bd87bd1
SUGIYAMA Yoshio <nenegi.01mo@gmail.com>
2024-04-23 16:13:30 +0900

    feat: multi query
    Signed-off-by: SUGIYAMA Yoshio <nenegi.01mo@gmail.com>

--- rag/app/main.py
+++ rag/app/main.py
@@ -1,8 +1,11 @@
 import logging
+from operator import itemgetter
 import os
 
 import bs4
 from langchain import hub
+from langchain.load import dumps, loads
+from langchain.prompts import ChatPromptTemplate
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders.web_base import WebBaseLoader
 from langchain_community.vectorstores.chroma import Chroma
@@ -16,7 +19,9 @@ def main():
 
     logging.basicConfig(level=logging.INFO)
 
-    #### INDEXING ####
+    ################################################################################
+    # INDEXING
+    ################################################################################
 
     # Load Documents
     loader = WebBaseLoader(
@@ -30,41 +35,93 @@ def main():
     docs = loader.load()
 
     # Split
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+        chunk_size=300, chunk_overlap=50
+    )
+
     splits = text_splitter.split_documents(docs)
 
     # Embed
     vectorstore = Chroma.from_documents(
         documents=splits, embedding=OpenAIEmbeddings(model=model_name)
     )
-
     retriever = vectorstore.as_retriever()
 
-    #### RETRIEVAL and GENERATION ####
+    ################################################################################
+    # GENERATE MULTI QUERIES
+    ################################################################################
 
     # Prompt
-    prompt = hub.pull("rlm/rag-prompt")
-
-    # LLM
-    llm = ChatOpenAI(model=model_name, temperature=0)
-
-    # Post-processing
-    def format_docs(docs):
-        return "\n\n".join(doc.page_content for doc in docs)
+    # Multi Query: Different Perspectives
+    # MultiQueryRetrieverは、LLMを用いて、与えられたユーザ入力クエリに対して異なる視点から複数のクエリを生成することで、
+    # プロンプトチューニングのプロセスを自動化する。
+    template = """You are an AI language model assistant. Your task is to generate five 
+    different versions of the given user question to retrieve relevant documents from a vector 
+    database. By generating multiple perspectives on the user question, your goal is to help
+    the user overcome some of the limitations of the distance-based similarity search. 
+    Provide these alternative questions separated by newlines. Original question: {question}"""
+    """
+    あなたはAI言語モデルアシスタントです。あなたの仕事は、ベクトル・データベースから関連文書を検索するために、
+    与えられたユーザーの質問に対して5つの異なるバージョンを生成することです。
+    ユーザの質問に対する複数の視点を生成することで、あなたのゴールは、
+    ユーザが距離ベースの類似検索の制限のいくつかを克服するのを助けることです。
+    改行で区切られたこれらの代替の質問を提供してください。
+    """
+    prompt_perspectives = ChatPromptTemplate.from_template(template)
 
     # Chain
-    rag_chain = (
-        {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    generate_queries = (
+        prompt_perspectives
+        | ChatOpenAI(model=model_name, temperature=0)
+        | StrOutputParser()
+        | (lambda x: x.split("\n"))
+    )
+
+    # Retrieve
+    question = "What is task decomposition for LLM agents?"
+    retrieval_chain = generate_queries | retriever.map() | get_unique_union
+
+    docs = retrieval_chain.invoke({"question": question})
+    logging.info(f"Number of retrieval documents: {len(docs)}")
+    logging.info(f"Retrieval documents: {docs}")
+
+    ################################################################################
+    # RAG
+    ################################################################################
+
+    # RAG
+    template = """Answer the following question based on this context:
+
+    {context}
+
+    Question: {question}
+    """
+
+    prompt = ChatPromptTemplate.from_template(template)
+
+    llm = ChatOpenAI(model=model_name, temperature=0)
+
+    final_rag_chain = (
+        {"context": retrieval_chain, "question": itemgetter("question")}
         | prompt
         | llm
         | StrOutputParser()
     )
 
-    # Question
-    result = rag_chain.invoke("What is Task Decomposition?")
+    result = final_rag_chain.invoke({"question": question})
 
     logging.info(f"Result: {result}")
-    logging.info("Done...")
+
+
+def get_unique_union(documents: list[list]):
+    """Unique union of retrieved docs"""
+    # Flatten list of lists, and convert each Document to string
+    flattened_docs = [dumps(doc) for sublist in documents for doc in sublist]
+    # Get unique documents
+    unique_docs = list(set(flattened_docs))
+    # Return
+    return [loads(doc) for doc in unique_docs]
+
 
 if __name__ == "__main__":
     main()
